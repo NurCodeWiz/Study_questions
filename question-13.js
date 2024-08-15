174df3382be820b67774a2c9c7467245093df3cc
Nur Unlu <ecz.ilknurunlu@gmail.com>
2024-08-14 22:46:16 -0700

    Number of Good Pairs

---
+++ question-13.js
@@ -0,0 +1,50 @@
+// 1512. Number of Good Pairs
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array of integers nums, return the number of good pairs.
+
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+// Example 2:
+
+// Input: nums = [1,1,1,1]
+// Output: 6
+// Explanation: Each pair in the array are good.
+// Example 3:
+
+// Input: nums = [1,2,3]
+// Output: 0
+
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+function numIdenticalPairs(nums) {
+    let count = 0;
+    let freqMap = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (freqMap[nums[i]]) {
+            count += freqMap[nums[i]];
+            freqMap[nums[i]]++;
+        } else {
+            freqMap[nums[i]] = 1;
+        }
+    }
+
+    return count;
+}
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3])); // Output: 4
+console.log(numIdenticalPairs([1, 1, 1, 1]));       // Output: 6
+console.log(numIdenticalPairs([1, 2, 3]));          // Output: 0
