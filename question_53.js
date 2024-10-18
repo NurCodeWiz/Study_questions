e140f04bd00eefb06dc0833d9edab6eb89efffc4
Nur Unlu <ecz.ilknurunlu@gmail.com>
2024-10-18 12:14:07 -0700

    Rescue Boat

---
+++ question_53.js
@@ -0,0 +1,30 @@
+
+// https://leetcode.com/problems/trapping-rain-water/description/
+var trap = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = 0;
+    let rightMax = 0;
+    let trappedWater = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                trappedWater += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                trappedWater += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+
+    return trappedWater;
+};
+s
