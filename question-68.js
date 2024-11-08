9c77c0b0344b11f143dbd8597fb69d4489ea0111
Nur Unlu <ecz.ilknurunlu@gmail.com>
2024-11-08 13:37:09 -0800

    Merge Two Sorted Lists java

--- question-68.js
+++ question-68.js
@@ -32,24 +32,27 @@
 // Both list1 and list2 are sorted in non-decreasing order.
 
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
 
-def mergeTwoLists(list1, list2):
-    dummy = ListNode(-1)
-    current = dummy
+function mergeTwoLists(list1, list2) {
+    const dummy = new ListNode(-1);
+    let current = dummy;
 
-    while list1 and list2:
-        if list1.val <= list2.val:
-            current.next = list1
-            list1 = list1.next
-        else:
-            current.next = list2
-            list2 = list2.next
-        current = current.next
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
 
-    current.next = list1 if list1 else list2
+    current.next = list1 || list2;
 
-    return dummy.next
+    return dummy.next;
+}
